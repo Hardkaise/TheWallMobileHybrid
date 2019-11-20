77bd65aae0986503210e3f5784a2dfc820894fad
Clément Nancelle <clement.nancelle@epitech.eu>
2019-11-20 09:49:52 +0100

    feat(build): adding missing prod dependancies

--- api/src/mongoose.ts
+++ api/src/mongoose.ts
@@ -4,13 +4,13 @@ import logger from './logger';
 
 export default function (app: Application) {
   mongoose.connect(
-    app.get('mongodb'),
+    process.env.MONGODB_URI || app.get('mongodb'),
     { useCreateIndex: true, useNewUrlParser: true }
   ).catch(err => {
     logger.error(err);
     process.exit(1);
   });
-  
+
   mongoose.Promise = global.Promise;
 
   app.set('mongooseClient', mongoose);
