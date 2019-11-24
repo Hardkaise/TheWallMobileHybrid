df1b12ff4943dec21dd5aef92bacab78ca0596f7
Clément Nancelle <clement.nancelle@epitech.eu>
2019-11-24 15:28:53 +0100

    feat(image-upload): removing size limit

--- api/src/app.ts
+++ api/src/app.ts
@@ -29,8 +29,13 @@ app.configure(configuration());
 app.use(helmet());
 app.use(cors());
 app.use(compress());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({
+  limit: '100mb'
+}));
+app.use(express.urlencoded({
+  limit: '100mb',
+  extended: true
+}));
 app.use(favicon(path.join(app.get('public'), 'favicon.ico')));
 // Host the public folder
 app.use('/', express.static(app.get('public')));
